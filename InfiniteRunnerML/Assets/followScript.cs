2c6bbf6eacc859b2de9d498d8f019014fb6d58ad
Rafael Bayer <rafibayer7@gmail.com>
2018-10-07 11:23:45 -0700

    added support for agent image

--- InfiniteRunnerML/Assets/followScript.cs
+++ InfiniteRunnerML/Assets/followScript.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class followScript : MonoBehaviour
 {
     public Transform target;
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
 
     private void Update()
     {
-        transform.position = target.position;
+        transform.position = target.position + offset;
     }
 
 }
