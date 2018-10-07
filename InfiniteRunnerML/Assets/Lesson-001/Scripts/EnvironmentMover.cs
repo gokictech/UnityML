a93fee12ed59fda7a2ffbf2b41db94d75c89555e
Tiago <tiago@somda.net>
2018-10-07 14:24:06 -0700

    added namespaces, fix init bug

--- InfiniteRunnerML/Assets/Lesson-001/Scripts/EnvironmentMover.cs
+++ InfiniteRunnerML/Assets/Lesson-001/Scripts/EnvironmentMover.cs
@@ -2,38 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvironmentMover : MonoBehaviour 
+namespace GOKiC
 {
-	public float speed = 0.3f;
-	Transform[] objectsToMove;
-	int childCount;
-	
-	void Start ()
-	{
-		
-	}
+    public class EnvironmentMover : MonoBehaviour
+    {
+        public float speed = 0.3f;
+		public bool updateListOnStart = false;
+        Transform[] objectsToMove;
+        int childCount;
 
-	void Update()
-	{
-		foreach(Transform obj in objectsToMove)
-		{
-			Move(obj);
-		}
-	}
+        void Start()
+        {
+			if(updateListOnStart)
+				UpdateListOfMoveObjects();
+        }
 
-	public void UpdateListOfMoveObjects()
-	{
-		childCount = transform.childCount;
-		
-		objectsToMove = new Transform[childCount];
-		for(int i = 0; i< childCount; i++)
-		{
-			objectsToMove[i] = transform.GetChild(i);
-		}
-	}
-	
-	public void Move(Transform objectToMove)
-	{
-		objectToMove.Translate(new Vector3(0f, 0f, speed));
-	}
+        void Update()
+        {
+			if(objectsToMove == null)
+				return;
+
+            foreach (Transform obj in objectsToMove)
+            {
+                Move(obj);
+            }
+        }
+
+        public void UpdateListOfMoveObjects()
+        {
+            childCount = transform.childCount;
+
+            objectsToMove = new Transform[childCount];
+            for (int i = 0; i < childCount; i++)
+            {
+                objectsToMove[i] = transform.GetChild(i);
+            }
+        }
+
+        public void Move(Transform objectToMove)
+        {
+            objectToMove.Translate(new Vector3(0f, 0f, speed));
+        }
+    }
 }
--- InfiniteRunnerML/Assets/Lesson-001/Scripts/EnvironmentSectionSpawner.cs
+++ InfiniteRunnerML/Assets/Lesson-001/Scripts/EnvironmentSectionSpawner.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvironmentSectionSpawner : MonoBehaviour
+namespace GOKiC
 {
-	public GameObject sectionPrefab;
-	public Vector3 offsetPosition;
+    public class EnvironmentSectionSpawner : MonoBehaviour
+    {
+        public GameObject sectionPrefab;
+        public Vector3 offsetPosition;
 
-	public int numberToSpawn;
+        public int numberToSpawn;
 
-	public bool startFromZero = true;
+        public bool startFromZero = true;
 
-	void Start () 
-	{
-		Vector3 startPosition = Vector3.zero;
-		for(int i = 0; i < numberToSpawn; i++)
-		{
-			startPosition = offsetPosition*(i);
-			if(startFromZero == false)
-			{
-				startPosition += offsetPosition;
-			}
-			Instantiate(sectionPrefab, offsetPosition*(i+1),Quaternion.identity, transform);
-		}
+        void Start()
+        {
+            Vector3 startPosition = Vector3.zero;
+            for (int i = 0; i < numberToSpawn; i++)
+            {
+                startPosition = offsetPosition * (i);
+                if (startFromZero == false)
+                {
+                    startPosition += offsetPosition;
+                }
+                Instantiate(sectionPrefab, offsetPosition * (i + 1), Quaternion.identity, transform);
+            }
 
-		var mover = GameObject.FindObjectOfType<EnvironmentMover>();
+            var mover = GameObject.FindObjectOfType<EnvironmentMover>();
 
-		mover.UpdateListOfMoveObjects();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+            mover.UpdateListOfMoveObjects();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
