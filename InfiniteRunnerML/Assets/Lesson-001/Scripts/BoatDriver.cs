3b78bfce057ea8da825572870b642ca66ac98ae8
Tiago <tiago@somda.net>
2018-10-07 15:08:43 -0700

    updated boat obj and refactored boat driver and movement

---
+++ InfiniteRunnerML/Assets/Lesson-001/Scripts/BoatDriver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOKiC
+{
+	public class BoatDriver : MonoBehaviour 
+	{
+		public float speed = 5f;
+		private Vector3 rightDirection = new Vector3(-1,0,0);
+		private Vector3 leftDirection = new Vector3(1,0,0);
+		BoatMovement movement;
+		float horizontal;
+		void Start () {
+			movement = GetComponent<BoatMovement>();
+		}
+		
+		void Update () 
+		{
+			horizontal = Input.GetAxis("Horizontal");
+		}
+
+		void FixedUpdate()
+		{
+			if(movement == null)
+			{
+				Debug.LogError("No BoatMovement found!");
+			}
+
+			if(horizontal < 0)
+			{
+				movement.Move(leftDirection, speed * Time.fixedDeltaTime);
+			}
+			else if(horizontal > 0)
+			{
+				movement.Move(rightDirection, speed * Time.fixedDeltaTime);
+			}
+		}
+	}
+}
---
+++ InfiniteRunnerML/Assets/Lesson-001/Scripts/BoatMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOKiC
+{
+    public class BoatMovement : MonoBehaviour
+    {
+
+		//private new Rigidbody rigidbody;
+        void Start()
+        {
+			//rigidbody = GetComponent<Rigidbody>();
+        }
+
+		public void Move(Vector3 direction, float speed)
+		{
+			transform.Translate(direction * speed);
+		}
+    }
+}
